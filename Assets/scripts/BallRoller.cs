6dcba91bccb456f126b1062106d480012c69ee41
Lars Pedersen <123490179+marsbarslars@users.noreply.github.com>
2024-02-02 16:26:59 -0600

    Make ball spiral

---
+++ Assets/scripts/BallRoller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BallRoller : MonoBehaviour
+{
+    public float speed;
+    private float r = 8f;
+    private float x;
+    private float z;
+    private float theta = 0f;
+    private int direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increment theta but keep it below 2pi
+        theta += 0.1f * speed * Time.deltaTime;
+        if (theta > 2f * Mathf.PI)
+        {
+            theta -= 2f * Mathf.PI;
+        }
+
+        // Change r but keep it between -8 and 8
+        r += 0.001f * speed * direction;
+        if (Mathf.Abs(r) > 8f)
+        {
+            r = 8f * direction;
+            direction *= -1;
+        }
+
+        x = r * Mathf.Cos(theta);
+        z = r * Mathf.Sin(theta);
+
+        transform.position = new Vector3(x, 0.5f, z);
+    }
+}
